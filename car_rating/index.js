58c49e9a5571f72edd1f0bacc50f6b8a898b0295
ParikhRahul3 <91833771+ParikhRahul3@users.noreply.github.com>
2021-10-03 19:06:42 +0530

    async function added angular code for car_rating

--- car_rating/index.js
+++ car_rating/index.js
@@ -13,6 +13,22 @@ exports.handler = async (event) => {
             rating = 3.5;
             break;
     }
+    
+    exports.handler = async (event) => {
+    console.log(event);
+    var rating = 3.25;
+    var manufacturer = event["manufacturer"];
+    switch(manufacturer) {
+        case "BMW":
+            rating = 4;
+            break;
+        case "Ferrari":
+            rating = 5;
+            break;
+        case "Porsche":
+            rating = 3.5;
+            break;
+    }
     const responseBody = {
         rating: rating
     }
